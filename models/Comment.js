55aec966b0ffa4c683a6b617287c2830b432ee6f
Mike <mikej.miller440@gmail.com>
2022-03-23 16:27:45 -0500

    created replies functionality

--- models/Comment.js
+++ models/Comment.js
@@ -27,19 +27,33 @@ const ReplySchema = new Schema(
 	}
 );
 
-const CommentSchema = new Schema({
-	writtenBy: {
-		type: String
+const CommentSchema = new Schema(
+	{
+		writtenBy: {
+			type: String
+		},
+		commentBody: {
+			type: String
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+			get: createdAtVal => dateFormat(createdAtVal)
+		},
+		// use ReplySchema to validate data for a reply
+		replies: [ReplySchema]
 	},
-	commentBody: {
-		type: String
-	},
-	createdAt: {
-		type: Date,
-		default: Date.now,
-		get: createdAtVal => dateFormat(createdAtVal)
-	},
-	replies: [ReplySchema]
+	{
+		toJSON: {
+			virtuals: true,
+			getters: true
+		},
+		id: false
+	}
+);
+
+CommentSchema.virtual('replyCount').get(function () {
+	return this.replies.length;
 });
 
 const Comment = model('Comment', CommentSchema);
